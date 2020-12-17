0600aad28a238d5b13a2c47ecf65334c7a0891ac
samsulhuda76 <61945288+samsulhuda76@users.noreply.github.com>
2020-12-17 22:35:42 +0700

    Add files via upload

---
+++ MainWindoController.cs
@@ -0,0 +1,36 @@
+﻿using Promos.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promos.Controller
+{
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void AddItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+        internal void addItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
